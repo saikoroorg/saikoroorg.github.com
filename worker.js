7d8eec2d0aa0347bdeba05f90ed3e60802f7f8f6
@ <@>
2019-06-01 08:57:46 +0900

    Release version 0.5.

--- worker.js
+++ worker.js
@@ -1,73 +1,4 @@
 // javascript
-// D1CE engine.
-
-
-// Namespace.
-var d1ce = d1ce || {};
-
-
-// Name.
-d1ce.name = "d1ce";
-
-// Version.
-d1ce.version = "0.4.190515+5";
-
-// Identifier.
-d1ce.identifier = d1ce.name + "-" + d1ce.version;
-
-// Timestamp.
-d1ce.timestamp = d1ce.version.match(/\d+\+\d+$/);
-
-
-// Engine class.
-d1ce.Engine = class {
-
-    // Wait time.
-    static async Wait(time) {
-        await d1ce.Count.Instance().Wait(time);
-    }
-
-    // Get time count.
-    static Time() {
-        return d1ce.Count.Instance().Time();
-    }
-
-    // Get random count.
-    static Random(max) {
-        return d1ce.Count.Instance().Random(max);
-    }
-
-    // Get parameters value.
-    static Value(key) {
-        return d1ce.Params.Instance().Value(key);
-    }
-
-    // Update parameters value.
-    static UpdateValue(key, value) {
-        d1ce.Params.Instance().UpdateValue(key, value);
-    }
-
-    // Print text to screen.
-    static Print(text) {
-        d1ce.Screen.Instance().Print(text);
-    }
-
-    // Clear screen.
-    static Clear() {
-        d1ce.Screen.Instance().Clear();
-    }
-
-    // Update input directions.
-    static async UpdateDirs() {
-        d1ce.Input.Instance().UpdateDirs();
-    }
-
-    // Get input directions.
-    static Dirs(raw=false) {
-        return d1ce.Input.Instance().Dirs(raw);
-    }
-}
-// javascript
 // Service worker for web app.
 
 
@@ -75,14 +6,14 @@ d1ce.Engine = class {
 const name = "d1ce";
 
 // Version.
-const version = "0.4.190515+5";
+const version = "0.5.190601+4";
 
 // Identifier.
 const identifier = name + "-" + version;
 
 // Cache files.
 // (need to set relative path "./" or absolute path "/")
-const cachefiles = ["./style.css", "./d1ce/app.html", "./d1ce/app.css", "./d1ce/cubes.png", "./d1ce/engine.js", "./d1ce/app.js"];
+const cachefiles = ["./style.css", "./d1ce/app.html", "./d1ce/app.css", "./d1ce/image/dot.png", "./d1ce/image/num.png", "./d1ce/image/card.png", "./d1ce/engine.js", "./d1ce/app.js"];
 
 
 // Event on installing service worker.
