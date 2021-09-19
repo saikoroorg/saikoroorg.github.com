f1d2f42def3a5864d77b58bfd83cb6697bda1bd4
@ <@>
2021-09-20 00:48:00 +0900

    Add subscreens for goke.

--- goban/manifest.js
+++ goban/manifest.js
@@ -1,7 +1,7 @@
 // Web app manifest for progressive web app.
 const manifest = {
     "name": "Goban",
-    "version": "0.8.10917d",
+    "version": "0.8.10919",
     "short_name": "Goban",
     "background_color": "#fff",
     "theme_color": "#fff",
