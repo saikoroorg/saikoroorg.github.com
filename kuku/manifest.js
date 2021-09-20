80d4406de6df57a373c92d96898f60699ad5a72a
@ <@>
2021-09-20 09:19:51 +0900

    Update icons.

--- kuku/manifest.js
+++ kuku/manifest.js
@@ -1,7 +1,7 @@
 // Web app manifest for progressive web app.
 const manifest = {
     "name": "Kuku",
-    "version": "0.8.10919",
+    "version": "0.8.10920",
     "short_name": "Kuku",
     "background_color": "#fff",
     "theme_color": "#fff",
