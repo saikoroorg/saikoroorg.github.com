b62ce0daaf5b6b7b33307c5b5fa216ad87669023
@ <@>
2021-09-22 01:56:23 +0900

    Fix touch reaction.

--- goban/manifest.js
+++ goban/manifest.js
@@ -1,7 +1,7 @@
 // Web app manifest for progressive web app.
 const manifest = {
     "name": "Goban",
-    "version": "0.8.10921",
+    "version": "0.8.10921a",
     "short_name": "Goban",
     "background_color": "#fff",
     "theme_color": "#fff",
