b4cb932650b5925da93e3f9bfd64bddb29a34a9e
@ <@>
2021-09-21 12:37:53 +0900

    Update goban menu.

--- goban/manifest.js
+++ goban/manifest.js
@@ -1,7 +1,7 @@
 // Web app manifest for progressive web app.
 const manifest = {
     "name": "Goban",
-    "version": "0.8.10920a",
+    "version": "0.8.10921",
     "short_name": "Goban",
     "background_color": "#fff",
     "theme_color": "#fff",
