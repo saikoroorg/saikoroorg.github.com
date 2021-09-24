2cdb3ebcd3ca32921fe9f1e6ae5510569e88184e
@ <@>
2021-09-25 02:23:05 +0900

    Revert "Fix focusing."
    This reverts commit 59029d4b1af1ce1a04a46fa95bf0ef8d51df4fe1.

--- goban/manifest.js
+++ goban/manifest.js
@@ -1,7 +1,7 @@
 // Web app manifest for progressive web app.
 const manifest = {
     "name": "Goban",
-    "version": "0.8.10921f",
+    "version": "0.8.10921e",
     "short_name": "Goban",
     "background_color": "#fff",
     "theme_color": "#fff",
