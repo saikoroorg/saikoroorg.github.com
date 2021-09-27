f16040a8594df56ef57ffea5d2f058abbae48e77
@ <@>
2021-09-27 18:20:17 +0900

    Fix overwrite bug.

--- goban/manifest.js
+++ goban/manifest.js
@@ -1,7 +1,7 @@
 // Web app manifest for progressive web app.
 const manifest = {
     "name": "Goban",
-    "version": "0.8.10921e",
+    "version": "0.8.10927",
     "short_name": "Goban",
     "background_color": "#fff",
     "theme_color": "#fff",
