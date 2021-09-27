1688539b712e57c1a390ede72da32bb032baf91b
@ <@>
2021-09-28 05:31:17 +0900

    Fix layout bug.

--- goban/manifest.js
+++ goban/manifest.js
@@ -1,7 +1,7 @@
 // Web app manifest for progressive web app.
 const manifest = {
     "name": "Goban",
-    "version": "0.8.10927b",
+    "version": "0.8.10927c",
     "short_name": "Goban",
     "background_color": "#fff",
     "theme_color": "#fff",
