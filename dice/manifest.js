ba36cf2fbdb835f6533d88e5436b42076ba6ae47
@ <@>
2021-10-02 15:36:36 +0900

    Use original face icon.

--- dice/manifest.js
+++ dice/manifest.js
@@ -1,7 +1,7 @@
 // Web app manifest for progressive web app.
 const manifest = {
     "name": "Dice",
-    "version": "0.8.11001",
+    "version": "0.8.11001a",
     "short_name": "Dice",
     "background_color": "#000",
     "theme_color": "#000",
