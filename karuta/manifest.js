91114341a991f08e00b52b9e3883e90c97fd263e
sak <sak@saikoro.org>
2022-03-31 03:26:16 +0900

    Add playboard.

--- karuta/manifest.js
+++ karuta/manifest.js
@@ -1,7 +1,7 @@
 // Web app manifest for progressive web app.
 const manifest = {
     "name": "Karuta",
-    "version": "0.8.20330",
+    "version": "0.8.20330a",
     "short_name": "Karuta",
     "author": "saikoro.org",
     "background_color": "#000",
