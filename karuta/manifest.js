d29ba01466fc6d39b94533eb9a23c58537d7acd1
sak <sak@saikoro.org>
2022-05-07 18:01:55 +0900

    Add rotate buttons.

--- karuta/manifest.js
+++ karuta/manifest.js
@@ -1,7 +1,7 @@
 // Web app manifest for progressive web app.
 const manifest = {
     "name": "Karuta",
-    "version": "0.8.20507",
+    "version": "0.8.20507a",
     "short_name": "Karuta",
     "author": "saikoro.org",
     "background_color": "#000",
